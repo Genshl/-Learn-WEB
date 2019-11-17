dc4a2a92ab8696f1179212b839fd21bbeb722cb0
Genshl <moskovolod@gmail.com>
2019-11-17 17:41:58 +0200

    ...

---
+++ php/V_1.php
@@ -0,0 +1,20 @@
+<?php
+
+    // define("HOST","localhost");   
+    // define("USER","root");
+    // define("PASS","");
+    // define("DB","firma");
+
+    // $mtsqli = @new mysqli(HOST,USER,PASS,DB);
+    // if ($mysgli->connect_errno) (
+    //     exit("помилка канекту");
+    // )else echo"до БД приконектився";     
+    // $mysgli->set_charset("utf-8");
+
+    // $result = $mysgli->querty("SELECT * FROM workers");
+    // echo"</br>";
+
+    // $mass = $result->fetch_assoc();
+    // echo $mass["name"].'має'.$mass["age"]."років";
+    // echo $mass["name"].'має'.$mass["age"]."років";
+?>
